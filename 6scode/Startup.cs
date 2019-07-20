cadd70d6c7fc6bebf3d065d9d35de9a1a7d3fd0a
jorzen1984 <277602146@qq.com>
2019-07-20 15:26:49 +0800

    初始化

---
+++ 6scode/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_6scode.Startup))]
+namespace _6scode
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
