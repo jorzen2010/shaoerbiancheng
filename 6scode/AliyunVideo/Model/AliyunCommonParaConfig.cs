58ef76fe6aacbc6a00ce75fde03269eeee4a723a
jorzen <jorzen2010@163.com>
2020-02-28 13:51:44 +0800

    Update AliyunCommonParaConfig.cs

--- 6scode/AliyunVideo/Model/AliyunCommonParaConfig.cs
+++ 6scode/AliyunVideo/Model/AliyunCommonParaConfig.cs
@@ -12,7 +12,7 @@ namespace AliyunVideo
         public const string Format = "JSON";
         public const string Version = "2017-03-21";
         public const string AccessKeyId = "LTAIHmoHhNfb1Euj";
-        public const string AccessKeySecret = "afCSAjYmNuVcPLTLuwxCtwvu4VvYrr";
+        public const string AccessKeySecret = "asfsCsSAjYsmNuVcsPLTLuwxCtswvu4VsvYrr";
       
         public const string SignatureVersion = "1.0";
 
